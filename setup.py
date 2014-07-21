30dae65b346561893ea3de88e00d1f0313b5857f
Priyanka Rani <priyanka.rani@openlabs.co.in>
2014-07-21 15:09:49 +0530

    Migrate to 3.2 #4943

--- setup.py
+++ setup.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
+import unittest
+import sys
 from setuptools import setup, Command
 import re
 import os
 import ConfigParser
+
+
+class SQLiteTest(Command):
+    """
+    Run the tests on SQLite
+    """
+    description = "Run tests on SQLite"
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from trytond.config import CONFIG
+        CONFIG['db_type'] = 'sqlite'
+        os.environ['DB_NAME'] = ':memory:'
+
+        from tests import suite
+        test_result = unittest.TextTestRunner(verbosity=3).run(suite())
+
+        if test_result.wasSuccessful():
+            sys.exit(0)
+        sys.exit(-1)
 
 
 class XMLTests(Command):
@@ -93,9 +121,7 @@ major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
 
-requires = [
-    'trytond_nereid>=3.0.7.0, < 3.1',
-]
+requires = []
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res|webdav)(\W|$)', dep):
         requires.append(
@@ -109,11 +135,6 @@ requires.append(
         major_version, minor_version, major_version, minor_version + 1
     )
 )
-tests_require = [
-    'trytond_product >= %s.%s, < %s.%s' % (
-        major_version, minor_version, major_version, minor_version + 1
-    )
-]
 
 setup(
     name='trytond_nereid_cms',
@@ -152,9 +173,9 @@ setup(
     """,
     test_suite='tests',
     test_loader='trytond.test_loader:Loader',
-    tests_require=tests_require,
     cmdclass={
         'xmltests': XMLTests,
         'audit': RunAudit,
+        'test': SQLiteTest,
     },
 )
